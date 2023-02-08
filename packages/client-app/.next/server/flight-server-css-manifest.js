35bb37e2d36ee870bdda552e7bad67cc3f6e66eb
Safin Ahmed <safin.ahmed2000@gmail.com>
2023-02-08 19:38:33 +0600

    initial commit

---
+++ packages/client-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "H:\\flexfit\\packages\\client-app\\app\\page": [
+      "H:\\flexfit\\packages\\client-app\\app\\page.module.css",
+      "H:\\flexfit\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"app\\\\page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "H:\\flexfit\\packages\\client-app\\app\\globals.css"
+    ]
+  },
+  "H:\\flexfit\\packages\\client-app\\app\\page.tsx": [
+    "H:\\flexfit\\packages\\client-app\\app\\page.module.css",
+    "H:\\flexfit\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"app\\\\page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "H:\\flexfit\\packages\\client-app\\app\\layout.tsx": [
+    "H:\\flexfit\\packages\\client-app\\app\\globals.css"
+  ]
+}
---
+++ packages/client-app/.next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-client)/../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cflexfit%5Cpackages%5Cclient-app%5Capp%5Cglobals.css&server=false!":
+/*!******************************************************************************************************************************************************************************!*\
+  !*** ../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cflexfit%5Cpackages%5Cclient-app%5Capp%5Cglobals.css&server=false! ***!
+  \******************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"_app-client_app_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./app/globals.css */ \"(app-client)/./app/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4uLy4uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1IJTNBJTVDZmxleGZpdCU1Q3BhY2thZ2VzJTVDY2xpZW50LWFwcCU1Q2FwcCU1Q2dsb2JhbHMuY3NzJnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxtTEFBdUYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz8xZjM5Il0sInNvdXJjZXNDb250ZW50IjpbIigoKSA9PiBpbXBvcnQoLyogd2VicGFja01vZGU6IFwibGF6eVwiICovIFwiSDpcXFxcZmxleGZpdFxcXFxwYWNrYWdlc1xcXFxjbGllbnQtYXBwXFxcXGFwcFxcXFxnbG9iYWxzLmNzc1wiKSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cflexfit%5Cpackages%5Cclient-app%5Capp%5Cglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cflexfit%5Cpackages%5Cclient-app%5Capp%5Cglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
