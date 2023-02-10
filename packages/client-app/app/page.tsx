e1923ecd0476b54e7a4cb58acb74261fa35dc32e
Omar Faruq <naimislam3545@gmail.com>
2023-02-11 03:09:57 +0600

    feat: progress page routing

--- packages/client-app/app/page.tsx
+++ packages/client-app/app/page.tsx
@@ -1,13 +1,16 @@
 import TestComponent from "@components/test";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+import PageHead from "@shared/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <TestComponent />
-    </main>
+    <>
+      <PageHead title="Home Page" />
+      <main>
+        <TestComponent />
+      </main>
+    </>
   );
 }
