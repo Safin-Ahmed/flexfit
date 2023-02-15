2e8d0549c9c42dbdcd36f16a0530c746f39f3bd2
Safin Ahmed <safin.ahmed2000@gmail.com>
2023-02-15 10:05:47 +0600

    chore: Fixed some problems in workout model.

--- packages/backend-app/config/database.js
+++ packages/backend-app/config/database.js
@@ -2,15 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", ),
-      port: env.int("DATABASE_PORT", ),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", ), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      schema: env("DATABASE_SCHEMA"), // Not required
     },
     debug: false,
   },
---
+++ packages/backend-app/src/api/user-exercise/routes/user-exercise.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-exercise router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-exercise.user-exercise');
