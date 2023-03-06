aefe5161808b0c48a8278f6503e6907f7142b53b
Omar Faruq <naimislam3545@gmail.com>
2023-03-06 19:43:41 +0600

    feat: profile page complete

--- packages/client-app/redux/features/api/profile/profileApi.ts
+++ packages/client-app/redux/features/api/profile/profileApi.ts
@@ -4,9 +4,18 @@ const profileApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUserProfile: builder.query<any, void>({
       query: () => "/users/me?populate=*",
+      providesTags: ["Profile"],
+    }),
+    updateProfile: builder.mutation({
+      query: ({ id, payload }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: payload,
+      }),
+      invalidatesTags: ["Profile"],
     }),
   }),
 });
 
-export const { useGetUserProfileQuery } = profileApi;
+export const { useGetUserProfileQuery, useUpdateProfileMutation } = profileApi;
 export default profileApi;
