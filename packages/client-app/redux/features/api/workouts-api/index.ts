23e208153692f4db6e1c397cffdab01775f77e0a
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-03-09 06:52:37 +0600

    fix: merge conflict resolved

--- packages/client-app/redux/features/api/workouts-api/index.ts
+++ packages/client-app/redux/features/api/workouts-api/index.ts
@@ -3,11 +3,7 @@ const workoutsApi = apiSlice.injectEndpoints({
   endpoints: (builder: any) => ({
     //@ts-ignore
     getAllWorkouts: builder.query<any, void>({
-<<<<<<< HEAD
-      query: () => 'workouts',
-=======
-      query: () => "/workouts?populate=deep",
->>>>>>> fe8b6f417bc5780fd521450478ba1c34ebe73002
+      query: () => '/workouts?populate=deep',
       transformResponse: (res: any) => {
         return res.data.sort((a: any, b: any) => b.id - a.id);
       },
@@ -15,53 +11,30 @@ const workoutsApi = apiSlice.injectEndpoints({
     }),
     //@ts-ignore
     getWorkout: builder.query<number, void>({
-<<<<<<< HEAD
       query: (workoutId: any) => `workouts/${workoutId}`,
     }),
     addWorkout: builder.mutation({
       query: (workout: any) => ({
         url: `workouts`,
         method: 'POST',
-=======
-      query: (workoutId) => `/workouts/${workoutId}`,
-    }),
-    addWorkout: builder.mutation({
-      query: (workout) => ({
-        url: `/workouts`,
-        method: "POST",
->>>>>>> fe8b6f417bc5780fd521450478ba1c34ebe73002
         body: workout,
       }),
       invalidatesTags: ['Workouts'],
     }),
     updateSingleWorkout: builder.mutation({
-<<<<<<< HEAD
       query: ({ workoutId, data }: any) => ({
         url: `workouts/${workoutId}`,
         method: 'PUT',
-=======
-      query: ({ workoutId, data }) => ({
-        url: `/workouts/${workoutId}`,
-        method: "PUT",
->>>>>>> fe8b6f417bc5780fd521450478ba1c34ebe73002
         body: data,
       }),
       invalidatesTags: ['Workouts'],
     }),
     deleteSingleWorkout: builder.mutation({
-<<<<<<< HEAD
       query: (id: any) => ({
         url: `workouts/${id}`,
         method: 'DELETE',
       }),
       invalidatesTags: ['Workouts'],
-=======
-      query: (id) => ({
-        url: `/workouts/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: ["Workouts"],
->>>>>>> fe8b6f417bc5780fd521450478ba1c34ebe73002
     }),
   }),
 });
