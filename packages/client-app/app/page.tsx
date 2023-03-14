2a3f9eaac50fd2e9215cf49d59a63507e6bbaba0
Safin Ahmed <safin.ahmed2000@gmail.com>
2023-03-14 21:28:28 +0600

    Added Login Form to the root page of frontend

--- packages/client-app/app/page.tsx
+++ packages/client-app/app/page.tsx
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
 "use client";
+
 import AuthForm from "@components/AuthForm";
 import { Container } from "@mui/material";
-import React from "react";
-=======
-'use client';
+import { Inter } from "@next/font/google";
 
-import Workouts from '@components/Customizable-Workout/Workouts';
-import { Inter } from '@next/font/google';
-import { useGetAllRoutinesQuery } from '@redux/features/api/routine-api';
-import { useGetAllWorkoutsQuery } from '@redux/features/api/workouts-api';
-import PageHead from '@shared/head';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
-  // const { data: allWorkouts } = useGetAllWorkoutsQuery();
-  // const { data: allRoutines } = useGetAllRoutinesQuery();
->>>>>>> ea113996ffa5b05c13bf3537e083903a45d00a37
+const inter = Inter({ subsets: ["latin"] });
 
 const AuthPage = () => {
   return (
